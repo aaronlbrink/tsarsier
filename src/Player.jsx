e77dcbc2a3898b6d3033c7584c4a5c285194b850
Aaron Brink <6415108+aaronlbrink@users.noreply.github.com>
2020-02-22 09:17:58 -0500

    Add client side GUI

--- src/Player.jsx
+++ src/Player.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 // import logo from './logo.svg';
 import "./App.css";
+import { Link } from 'react-router-dom';
 // import { Container, Stage, Sprite, useTick } from "@inlet/react-pixi";
 // import {
 //   BrowserRouter as Router,
@@ -14,7 +15,18 @@ const Player = () => {
 
   return (
 <>
-<p>Player</p>
+  <Link to="/">Toggle Web</Link>
+  <div className="button" onPress={() => {  }} style={{margin: 5, width: '100%', height: '30vh', backgroundColor: 'grey', display: 'flex', alignItems: 'center', justifyContent: 'center'}}>
+    FIRE
+  </div>
+  <div style={{display: 'flex'}}>
+    <div className="button" onPress={() => {  }} style={{margin: 5, width: '100%', height: '30vh', backgroundColor: 'grey', display: 'flex', alignItems: 'center', justifyContent: 'center'}}>
+      /|
+    </div>
+    <div className="button" onPress={() => {  }} style={{margin: 5, width: '100%', height: '30vh', backgroundColor: 'grey', display: 'flex', alignItems: 'center', justifyContent: 'center'}}>
+      |/
+    </div>
+  </div>
 </>
   )
 }
