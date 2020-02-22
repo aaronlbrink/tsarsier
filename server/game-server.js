63b287142980abc03ce02b3ec4fb391ca3e93c8d
Kristofer Paul Gunadi Brink <kpgbrink@gmail.com>
2020-02-22 11:41:34 -0500

    game logic

--- server/game-server.js
+++ server/game-server.js
@@ -1,3 +1,97 @@
 const util = require('./util');
 const config = require('../src/config.json');
-const matterjs = require('matter-js');
+const Matter = require('matter-js');
+
+var Engine = Matter.Engine,
+    World = Matter.World,
+    Bodies = Matter.Bodies;
+
+/*
+Data sent to screen:
+    After round input:
+        All the frames of the round
+    What team won the game
+    When game going
+
+Data sent to phone
+    At the beginning of game:
+        team num
+    On new round:
+        round count
+
+Data sent from phone:
+    either
+    fireInput: angle, magnitude 
+    or
+    moveInput: float
+
+*/
+
+
+module.exports = class GameServer {
+    constructor() {
+
+    }
+
+    checkEndGame() {
+        // Check if all players left are on the same team
+        // or no players left
+        
+    }
+    
+    resetValuesOnNewGame() {
+        this.roundCount = 0;
+    }
+
+    destroyPreviousGame() {
+        
+    }
+
+    gameStart() {
+        // Has to run first
+        destroyPreviousGame();
+
+        resetValuesOnNewGame();
+        this.generateTerrain();
+        this.generatePlayers();
+    }
+
+    generateTerrain() {
+        // Make the terrain
+        // Calculate the terrain 
+        let terrain = config.game.terrain;
+        let generation = terrain.generation;
+        var indexGenerating = 0;
+        this.heights = [];
+        while (indexGenerating < terrain.width/2) {
+            heights.push(terrain.minHeight);
+        }
+        // While we haven't gotten to the end of the terrain
+
+    }
+
+    generatePlayers() {
+        this.randomlyAssignTeam();
+        // Make all of the bodys for each player and spawn them randomly on their side of the game
+    }
+
+    randomlyAssignTeam() {
+        // Randomly pick a user from the list
+        // assign to team 1 then to alternating
+        let teamNumber = 1;
+        let usersToAssign = [...this.users];
+        while (usersToAssign.length > 0) {
+            var index = utils.randomIntBetween(0, usersToAssign.length -1);
+            let userAssigning = usersToAssign[index];
+            // TODO make this actually assign it to the team number
+            
+            array.splice(index, 1);
+            teamNumber = ((teamNumber + 1) % config.game.teams.numberOfTeams) + 1;
+        }
+    }
+
+    roundAnimate() {
+        // 
+    }
+
+}
--- server/utils.js
+++ server/utils.js
@@ -2,7 +2,14 @@ exports.randomInt = (x) => {
     return Math.floor(Math.random() * x);
   }
   
-  exports.randomColor = () => {
-      return Math.floor(Math.random()*16777215);
-  };
-  
+exports.randomColor = () => {
+    return Math.floor(Math.random()*16777215);
+};
+
+// both inclusive
+exports.randomIntBetween = (min, max) => {
+    max++;
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min) + min);
+}
