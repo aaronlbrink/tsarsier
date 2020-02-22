16debfa1c14fee1e57e9f9ad9ee4d5435e5f9848
Peter VandeHaar <pjvandehaar@gmail.com>
2020-02-22 01:57:43 -0500

    Add quick socketio user management

--- server.js
+++ server.js
@@ -1,8 +1,40 @@
 var app = require('express')();
 var http = require('http').createServer(app);
+var io = require('socket.io')(http);
 
 app.get('/', function(req, res){
   res.send('<h1>Hello world</h1>');
+});
+
+var users = [];
+var last_user_id = 0;
+
+io.on('connection', (socket) => {
+  var addedUser = false;
+
+  socket.on('add user', (username) => {
+    if (addedUser) return; // idempotent
+    addedUser = true;
+    console.log(`adding user ${username} (users were ${JSON.stringify(users)})`);
+    socket.username = username;
+    socket.user_id = ++last_user_id;
+    users.push({username: socket.username, user_id: socket.user_id});
+    socket.emit('userlist', { users: users});
+    socket.broadcast.emit('user joined', {
+      username: socket.username,
+      users: users,
+    });
+  });
+
+  socket.on('submit move', (move) => {
+    console.log(`user chose to move ${JSON.stringify(move)}`);
+  })
+
+  socket.on('disconnect', () => {
+    var idx = users.findIndex(d => d.user_id === socket.user_id);
+    if (idx >= 0) users.splice(idx, 1);
+    console.log(`removed user ${socket.username} with id ${socket.user_id} (users are ${JSON.stringify(users)})`);
+  });
 });
 
 http.listen(3001, function(){
