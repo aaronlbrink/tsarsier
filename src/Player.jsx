25d4c4d20f3a774c0fea0fc0997b3f35643e193d
Aaron Brink <6415108+aaronlbrink@users.noreply.github.com>
2020-02-22 01:52:00 -0500

    See name

--- src/Player.jsx
+++ src/Player.jsx
@@ -1,4 +1,4 @@
-import React, { useReducer, useRef } from "react";
+import React from "react";
 // import logo from './logo.svg';
 import "./App.css";
 // import { Container, Stage, Sprite, useTick } from "@inlet/react-pixi";
