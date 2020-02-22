5d7c43c7fb78ceb7d10f64fa9b8f70c6765b72c5
Aaron Brink <6415108+aaronlbrink@users.noreply.github.com>
2020-02-21 21:01:51 -0500

    Frontend intialization

---
+++ packages/frontend/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <canvas id="tutorial" width="150" height="150"></canvas>
+    </div>
+  );
+}
+
+export default App;
