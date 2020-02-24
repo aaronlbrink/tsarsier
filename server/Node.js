4d87c8ce8c5e33941200fbf28329266a289f780e
aaronlbrink <aaron@brinkdatabase.com>
2020-02-23 20:32:16 -0500

    Add check if point is in polygon

--- server/Node.js
+++ server/Node.js
@@ -34,8 +34,9 @@ module.exports = class Node {
 
   // point: [8, 4]
   // polygon: [  [0, 2], [4, 8], [9, 7]  ]
-  checkIfPointIsInPolygon(point, polygon = []) {
-    console.log('start check if point is in polygon')
+  isPointInPolygon(point, polygon = []) {
+    let intersections = 0;
+    console.log('start check if point is in polygon');
     // Iterate over number of lines in polygon (number of points divided by two, plus the connecting line at between the end node and the first node)
     for (let pointIndex = 0; pointIndex < polygon.length; pointIndex++ ) {
       console.log('--LINE #' + (pointIndex + 1) + '--')
@@ -50,15 +51,52 @@ module.exports = class Node {
       }
       console.log('Using line points: ' + linePoint1 + ' and ' + linePoint2)
 
+      // Invalid Scenarios
+      // According to the UofM article (cited further below), there are some instances of lines which should not be counted
+      // as intersections. These include horizontal lines, line vertices lying on the imaginary ray
+      // Horizontal lines
+      if (linePoint2[1] - linePoint1[1] === 0) {
+        console.log('EDGE CASE: the line is horizontal');
+      }
+      // Line vertex is on imaginary ray
+      if (linePoint1[1] === point[1] || linePoint2[1] === point[1]) {
+        console.log('EDGE CASE: one of the line vertices started on the imaginary ray');
+        continue;
+      }
+
       // Test range match: point is between linePoint1's value and linePoint2's value
       if (point[1] > Math.min(linePoint1[1], linePoint2[1]) && point[1] < Math.max(linePoint1[1], linePoint2[1])) {
         // Range matched!
-        console.log('the range matched!')
-        // Test domain match: 
+        
+        // Continue to domain tests:
+        if (linePoint2[0] - linePoint1[0] === 0) {
+          // Slope is inifinity, Test as a vertical line 
+          // Test domain match: Grab any [x, y] pair (since x is the same for all y's) and check if x is after the test point's x
+          if (linePoint1[0] > point[0]) {
+            // There must be an intersection along the imaginary line, parellel to the x-axis, going right from the test point
+            console.log('INTERSECT: Vertical Line')
+            intersections++;
+            continue;
+          }
+        }
         const m = ((linePoint2[1] - linePoint1[1]) / (linePoint2[0] - linePoint1[0]));
         const b = linePoint1[1] - (m * linePoint1[0]);
+        const x = (point[1] - b) / m;
         console.log('slope: ' + m + ' y-intercept: ' + b);
+
+        // Test domain match: 
+        if (x > point[0]) {
+          console.log('INTERSECT: Line with slope')
+          // Based on the conditions, there must have been an intersection over an imaginary line extending from the point, parallel to the x-axis, continuing right.
+          // We add this intersection to the intersection count, according to the UMich site,
+          // http://www.eecs.umich.edu/courses/eecs380/HANDOUTS/PROJ2/InsidePoly.html
+          // if there are an even number of intersections, the point is outside the polygon
+          intersections++;
+        }
       }
+      console.log('-- ---')
     }
+    console.log('total line intersections: ' + intersections)
+    return intersections % 2 === 1;
   }
 }
